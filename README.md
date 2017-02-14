5917e7f220037db5aef014a3b9bbea1dfcca4f30
parasquid <tristan.gomez@gmail.com>
2017-02-14 18:16:28 +0800

    implement the baseline program
    count the number of primes between a range, sequentially testing
    each number in the range for primality

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # concurrency-paradigms
+
+The baseline "workload" will be the classic:
+
+    Count the number of primes between 1 to n
+
+but implemented in a sequential fashion.
+
+In my machine running Windows 10 WSL (Ubuntu 16.04):
+
+```
+[~/parasquid/concurrency-paradigms] (master) tristan$ time ruby primes.rb
+
+real  0m25.124s
+user  0m25.016s
+sys   0m0.109s
+```
---
+++ primes.rb
@@ -0,0 +1,39 @@
+class Prime
+  # check for primality
+  # see https://en.wikipedia.org/wiki/Primality_test#Pseudocode
+  def self.is_prime(n)
+    return false if n <= 1
+    return true if n <= 3
+    return false if n % 2 == 0
+    return false if n % 3 == 0
+
+    i = 5 # start off with 5 because we've dealt with 2..4
+    while i * i <= n
+      return false if n % i == 0
+      return false if n % (i + 2) == 0
+      i = i + 6
+    end
+
+    true # fell through sieve, must be prime
+  end
+
+  def initialize(start:2, up_to:)
+    @start = start
+    @end = up_to
+  end
+
+  def list_primes
+    primes = []
+    (@start..@end).each do |n|
+      primes << n if self.class.is_prime(n)
+    end
+    primes
+  end
+
+  def count_primes
+    list_primes.count
+  end
+end
+
+p = Prime.new(up_to: 10_000_000)
+p.count_primes
