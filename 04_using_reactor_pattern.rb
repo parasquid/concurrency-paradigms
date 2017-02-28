5ce2e4b6e29ce10066444f0c011684ad40124b16
parasquid <tristan.gomez@gmail.com>
2017-02-28 15:57:22 +0800

    implement properly the reactive pattern
    previous version doesn't really implement the reactor pattern that well.
    
    this one does and looks a lot more concurrent.

--- 04_using_reactor_pattern.rb
+++ 04_using_reactor_pattern.rb
@@ -2,49 +2,41 @@ require "eventmachine"
 require_relative "numeric"
 
 EM.run {
-  work = (FIRST..LAST)
+  work = (FIRST..LAST).to_a
   primes = []
   palindromes = []
   messages = []
 
   calculate_primes = ->{
-    puts "calculate_primes"
-    primes = work.map { |n|
-      n if n.is_prime?
-    }.compact
+    n = work.shift
+    primes << n if n && n.is_prime?
   }
 
   calculate_palindromes = ->{
-    puts "calculate_palindromes"
-    palindromes = primes.map { |n|
-      n if n.is_palindrome?
-    }.compact
+    n = primes.shift
+    palindromes << n if n && n.is_palindrome?
   }
 
   print_messages = ->{
-    palindromes.map { |n| "#{n} is a palindromic prime" }
+    n = palindromes.shift
+    puts n if n
   }
-
-  puts "deferring prime calculation"
-  EM.defer(calculate_primes, ->(primes) {
-    puts "deferring palindrome calculation"
-    EM.defer(calculate_palindromes, ->(palindromes) {
-      puts "deferring message printing"
-      EM.defer(print_messages, ->(messages) {
-        puts messages
-        EM.stop
-      })
-      puts "message printing deferred"
-    })
-    puts "palindrome calculation deferred"
-  })
-  puts "prime calculation deferred"
 
   timer = 0
   EM.add_periodic_timer(1) {
-    puts timer += 1
+    puts "tick! #{timer += 1}"
   }
 
+  tickloop = EM.tick_loop do
+    if work.empty?
+      :stop
+    else
+      EM.next_tick(calculate_primes)
+      EM.next_tick(calculate_palindromes)
+      EM.next_tick(print_messages)
+    end
+  end
+  tickloop.on_stop { EM.stop }
 }
 
 puts "all done!"
