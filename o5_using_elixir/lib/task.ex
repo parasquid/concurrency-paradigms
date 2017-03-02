fd95174b187b0830b400afb3ae83255e125762e4
Wei Lih Loh <willie@mindvalley.com>
2017-03-02 22:47:05 +0800

    prime_palindrom/1 palindrom_prime/1

--- o5_using_elixir/lib/task.ex
+++ o5_using_elixir/lib/task.ex
@@ -1,7 +1,30 @@
 defmodule Task do
   def run(last, max \\ 20) do
     Task.Supervisor.async_stream(TaskSupervisor, Enum.to_list(1..last),
-                                 Number, :is_prime?, [], max_concurrency: max)
-    |> Enum.to_list()
+                                 &prime_palindrome/1, max_concurrency: max)
+    |> Stream.filter(fn({:ok, i}) -> is_integer(i) end)
+    |> Enum.to_list
   end
+
+  defp prime_palindrome(number) do
+    number = number |> prime_value() |> palindrome_value()
+    if is_integer(number), do: IO.puts number
+    number
+  end
+
+  defp palindrome_prime(number) do
+    number = number |> palindrome_value() |> prime_value()
+    if is_integer(number), do: IO.puts number
+    number
+  end
+
+  defp prime_value(number) when is_integer(number) do
+    if Number.is_prime?(number), do: number, else: nil
+  end
+  defp prime_value(_), do: nil
+
+  defp palindrome_value(number) when is_integer(number) do
+    if Number.is_palindrome?(number), do: number, else: nil
+  end
+  defp palindrome_value(_), do: nil
 end
