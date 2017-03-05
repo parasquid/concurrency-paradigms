c83d2fe26f91d231788c301a257e464ae271da0c
parasquid <tristan.gomez@gmail.com>
2017-03-06 00:18:28 +0800

    use a better example of the actor model
    this one has the mailbox and the linking easily seen.
    
    bonus for the await signal that we can catch

--- 05_using_actors.rb
+++ 05_using_actors.rb
@@ -1,47 +1,49 @@
-require 'celluloid'
+require "concurrent"
+require "concurrent-edge"
 require_relative "numeric"
 
-WORKER_COUNT = 4
-
-class WorkQueue
-  include Celluloid
-  attr_reader :queue
-
+class Worker < Concurrent::Actor::RestartingContext
   def initialize(out:)
     @out = out
   end
-
 end
 
-class MessagePrinter < WorkQueue
-  def call(n)
-    puts n
+class MessagePrinter < Worker
+  def on_message(n)
+    puts "#{n} is a palindromic prime"
   end
 end
 
-class PalindromeCalculator < WorkQueue
-  def call(n)
-    @out.async.call(n) if n.is_palindrome?
+class PalindromeCalculator < Worker
+  def on_message(n)
+    @out.tell(n) if n.is_palindrome?
   end
 end
 
-class PrimeCalculator < WorkQueue
-  def call(n)
-    @out.async.call(n) if n.is_prime?
+class PrimeCalculator < Worker
+  def on_message(n)
+    @out.tell(n) if n.is_prime?
   end
 end
 
-class NumberFeeder < WorkQueue
-  def call(range)
-    range.each { |n| @out.async.call(n) }
+class NumberFeeder < Worker
+  def on_message(range)
+    range.each do |n|
+      @out.tell(n)
+    end
   end
 end
 
-messages = MessagePrinter.new(out: nil)
-palindromes = PalindromeCalculator.new(out: messages)
-primes = PrimeCalculator.new(out: palindromes)
-feeder = NumberFeeder.new(out: primes)
+messages = MessagePrinter.spawn(name: :messages, link: true, args: [{out: nil}])
+palindromes = PalindromeCalculator.spawn(name: :palindromes, link: true, args: [{out: messages}])
+primes = PrimeCalculator.spawn(name: :primes, link: true, args: [{out: palindromes}])
+feeder = NumberFeeder.spawn(name: :feeder, link: true, args: [{out: primes}])
 
-feeder.call(FIRST..LAST)
+feeder.tell((FIRST..LAST))
 
-sleep
+feeder.ask(:await).wait
+primes.ask(:await).wait
+palindromes.ask(:await).wait
+messages.ask(:await).wait
+
+puts "all done!"
